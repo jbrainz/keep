7c6c159581b5e73c5b7faa9ff1f1b788e1177a5a
brainz <piusunimke@gmail.com>
2020-10-03 17:04:16 +0100

    user register auto login implemented on the Home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
+
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  const {loadUser} = authContext;
+  
+  useEffect(()=>{
+    loadUser();
+  }, [])
+  
   return (
-    <div className="grid-2">
+    <div className='grid-2'>
       <div>
-          <ContactForm/>
+        <ContactForm />
       </div>
       <div>
-        <ContactFilter/>
-        <Contacts/>
+        <ContactFilter />
+        <Contacts />
       </div>
     </div>
   );
