0c739a08e7234cde74fcd236df238c0260121f96
brainz <piusunimke@gmail.com>
2020-10-03 17:05:20 +0100

    final commit for state handlers

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,110 @@
+import React, { useReducer } from 'react';
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+import axios from 'axios';
+import setAuthToken from '../../util/setAuthToken';
+import {
+  REGISTER_SUCCES,
+  REGISTER_FAIL,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  //Register User.
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.post('/api/users', formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCES,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
+
+  //Load user.
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
+
+  //Login User.
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
+
+  //Logout User.
+
+  //Clear Errors.
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        error: state.error,
+        user: state.user,
+        register,
+        loadUser,
+        clearErrors,
+        login,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
